7910c108a0356376f499128988c3facb0c3ff9d1
Cleyton <cleytonjosedesouza@hotmail.com>
2024-06-20 15:24:00 -0300

    Agrupar os funcionários por função em um MAP, sendo a chave a “função” e o valor a “lista de funcionários”.

--- industria/src/main/java/industria/Principal.java
+++ industria/src/main/java/industria/Principal.java
@@ -6,8 +6,10 @@ import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
+import java.util.Map;
 
 public class Principal {
     public static void main(String[] args) {
@@ -15,13 +17,14 @@ public class Principal {
 
         
         DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-    
+        
         DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-
+        
         NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.forLanguageTag("pt-BR"));
         numberFormat.setMinimumFractionDigits(2);
         numberFormat.setMaximumFractionDigits(2);
 
+        
         funcionarios.add(new Funcionario("Maria", LocalDate.parse("18/10/2000", inputFormatter), new BigDecimal("2009.44"), "Operador"));
         funcionarios.add(new Funcionario("João", LocalDate.parse("12/05/1990", inputFormatter), new BigDecimal("2284.38"), "Operador"));
         funcionarios.add(new Funcionario("Caio", LocalDate.parse("02/05/1961", inputFormatter), new BigDecimal("9836.14"), "Coordenador"));
@@ -33,18 +36,37 @@ public class Principal {
         funcionarios.add(new Funcionario("Heloísa", LocalDate.parse("24/05/2003", inputFormatter), new BigDecimal("1606.85"), "Eletricista"));
         funcionarios.add(new Funcionario("Helena", LocalDate.parse("02/09/1996", inputFormatter), new BigDecimal("2799.93"), "Gerente"));
 
+        
         funcionarios.removeIf(funcionario -> funcionario.getNome().equals("João"));
-
-        System.out.println("Nome - Data Nascimento - Salário - Função");
-        System.out.println("----------------------------------------");
 
         
         for (Funcionario funcionario : funcionarios) {
-            String dataFormatada = funcionario.getDataNascimento().format(outputFormatter);
-            String salarioFormatado = numberFormat.format(funcionario.getSalario());
+            BigDecimal salarioAtual = funcionario.getSalario();
+            BigDecimal aumento = salarioAtual.multiply(BigDecimal.valueOf(0.10));
+            BigDecimal novoSalario = salarioAtual.add(aumento);
+            funcionario.setSalario(novoSalario);
+        }
 
-            System.out.println(funcionario.getNome() + " - " + dataFormatada + " - " 
-                + salarioFormatado + " - " + funcionario.getFuncao());
+        
+        Map<String, List<Funcionario>> funcionariosPorFuncao = new HashMap<>();
+        for (Funcionario funcionario : funcionarios) {
+            funcionariosPorFuncao.computeIfAbsent(funcionario.getFuncao(), k -> new ArrayList<>()).add(funcionario);
+        }
+
+        
+        for (Map.Entry<String, List<Funcionario>> entry : funcionariosPorFuncao.entrySet()) {
+            System.out.println("________________________________________");
+            System.out.println("Função: " + entry.getKey());
+            System.out.println("----------------------------------------");
+
+            for (Funcionario funcionario : entry.getValue()) {
+                String dataFormatada = funcionario.getDataNascimento().format(outputFormatter);
+                String salarioFormatado = numberFormat.format(funcionario.getSalario());
+
+                System.out.println(funcionario.getNome() + " - " + dataFormatada + " - " + salarioFormatado + " - " + funcionario.getFuncao());
+            }
+
+            System.out.println();
         }
     }
 }
