68d6689592923d61e76750f5a391f15ba4d451f6
Cleyton <cleytonjosedesouza@hotmail.com>
2024-06-20 14:44:24 -0300

    Inserir todos os funcionários, na mesma ordem e informações da tabela acima.

---
+++ industria/src/main/java/industria/Principal.java
@@ -0,0 +1,43 @@
+package industria;
+
+import industria.pessoas.Funcionario;
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Principal {
+    public static void main(String[] args) {
+        List<Funcionario> funcionarios = new ArrayList<>();
+
+        
+        DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    
+        DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.forLanguageTag("pt-BR"));
+        numberFormat.setMinimumFractionDigits(2);
+        numberFormat.setMaximumFractionDigits(2);
+
+        funcionarios.add(new Funcionario("Maria", LocalDate.parse("18/10/2000", inputFormatter), new BigDecimal("2009.44"), "Operador"));
+        funcionarios.add(new Funcionario("João", LocalDate.parse("12/05/1990", inputFormatter), new BigDecimal("2284.38"), "Operador"));
+        funcionarios.add(new Funcionario("Caio", LocalDate.parse("02/05/1961", inputFormatter), new BigDecimal("9836.14"), "Coordenador"));
+        funcionarios.add(new Funcionario("Miguel", LocalDate.parse("14/10/1988", inputFormatter), new BigDecimal("19119.88"), "Diretor"));
+        funcionarios.add(new Funcionario("Alice", LocalDate.parse("05/01/1995", inputFormatter), new BigDecimal("2234.68"), "Recepcionista"));
+        funcionarios.add(new Funcionario("Heitor", LocalDate.parse("19/11/1999", inputFormatter), new BigDecimal("1582.72"), "Operador"));
+        funcionarios.add(new Funcionario("Arthur", LocalDate.parse("31/03/1993", inputFormatter), new BigDecimal("4071.84"), "Contador"));
+        funcionarios.add(new Funcionario("Laura", LocalDate.parse("08/07/1994", inputFormatter), new BigDecimal("3017.45"), "Gerente"));
+        funcionarios.add(new Funcionario("Heloísa", LocalDate.parse("24/05/2003", inputFormatter), new BigDecimal("1606.85"), "Eletricista"));
+        funcionarios.add(new Funcionario("Helena", LocalDate.parse("02/09/1996", inputFormatter), new BigDecimal("2799.93"), "Gerente"));
+
+        for (Funcionario funcionario : funcionarios) {
+            String dataFormatada = funcionario.getDataNascimento().format(outputFormatter);
+            String salarioFormatado = numberFormat.format(funcionario.getSalario());
+
+            System.out.println(funcionario.getNome() + " - " + dataFormatada + " - " + salarioFormatado + " - " + funcionario.getFuncao());
+        }
+    }
+}
